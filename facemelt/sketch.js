85059cf2afd4cc456a51f12f4ee43dfa8c3edc00
Rayhan Moidu <54902342+rayhanmoidu@users.noreply.github.com>
2024-03-30 12:30:35 -0400

    melting is kind of working now

--- facemelt/sketch.js
+++ facemelt/sketch.js
@@ -3,9 +3,23 @@
  *   Mix of WEBGL 3D and normal 2D p5 rendering modes.
  */
 
+let s = {
+  fx: 0,
+  fxMax: 5,
+  fxMin: -5,
+  fxStep: 0.001,
+
+  fy: 0,
+  fyMax: 5,
+  fyMin: -5,
+  fyStep: 0.001,
+}
+
 let buffer3D;
 let shaderBuffer;
 let bust;
+
+let meltedFace;
 
 let canvas;
 let tex;
@@ -13,71 +27,162 @@ let tex;
 // Playing with shaders!
 let myShader; 
 
+let x0;
+let a, b;
+let mu, v;
+let epsilon;
+let f;
+
 function preload() {
   myShader = loadShader('data/sketch.vert', 'data/sketch.frag');
   bust = loadImage('data/bust.png')
 }
 
 function setup() {
+  createParamGui(s, paramChanged);
   canvas = createCanvas(windowWidth, windowHeight, WEBGL);
 
-  shaderBuffer = createGraphics(470*4, 531*4, WEBGL);
+  // shaderBuffer = createGraphics(470*4, 531*4, WEBGL);
   // need to set the "renderer" to WEBGL
-  buffer3D = createGraphics(470*4, 531*4, WEBGL);
+  // buffer3D = createGraphics(470*4, 531*4, WEBGL);
+
+  meltedFace = createGraphics(bust.width, bust.height, WEBGL);
   // Workaround for bug: https://github.com/processing/p5.js/issues/5634
   // Enables 3D graphics buffers to have transparent background.
-  buffer3D.setAttributes("alpha", true);
-  shaderBuffer.setAttributes("alpha", true);
+  meltedFace.setAttributes("alpha", true);
 
-  calcBuffer3D();
+  bust.loadPixels();
+
+
+  x0 = new Vec2(meltedFace.width/2, meltedFace.height/2);
+  f = new Vec2(s.fx, s.fy);
+  epsilon = 7;
+  mu = 	0.0006;
+  v = 0.5;
+  a = 1 / (4 * PI * mu);
+  b = a / (4 * (1 - v));
+
+  this.t = 0;
+
+  // buffer3D.setAttributes("alpha", true);
+  // shaderBuffer.setAttributes("alpha", true);
+
+  // calcBuffer3D();
 
   noCursor();
 }
 
-function calcBuffer3D() {
-  // IMPORTANT!: Needed in order to clear depth buffer
-  buffer3D.clear();
-  buffer3D.background(100);
-  // Note: Can draw a background here with buffer3D.background(...)
-  //    if transparency is not desired.
+function getKelvinlet(r) {
+  let r_len = r.length2();
+  let r_e = sqrt(r_len*r_len + epsilon*epsilon);
+  let r_e_3 = r_e * r_e * r_e;
 
-  // Need push() and pop() here because transformations don't get
-  //   reset each frame.
-  buffer3D.push();
-  // (0,0,0) is at the centre of the canvas
-  // buffer3D.translate(0, cos(r)*(100/factor), 0);
-  // buffer3D.rotateY(r);
-  // buffer3D.rotateX(r);
-  // buffer3D.rotateZ(-r);
-  // buffer3D.texture(textureBuffer);
-  // buffer3D.normalMaterial();
-  // buffer3D.strokeWeight(3);
-  // draw a 100 by 100 by 100 3D cube
-  // buffer3D.scale(0.8)
-  buffer3D.image(bust, 0, 0);
-  buffer3D.pop();
+  let term1 = (a - b) / r_e;
+  let term2 = b / (r_e_3);
+  let term3 = (a / 2) * ((epsilon*epsilon)/r_e_3);
 
-  // r += 0.01;
+  let rrt = [r.scalarmult(r.getX()), r.scalarmult(r.getY())];
+
+  rrt[0] = rrt[0].scalarmult(term2);
+  rrt[1] = rrt[1].scalarmult(term2);
+
+  rrt[0].setX(rrt[0].getX() + term1 + term3);
+  rrt[1].setY(rrt[1].getY() + term1 + term3);
+
+  // print(rrt[0], rrt[1])
+
+  let final = new Vec2(rrt[0].dot(f), rrt[1].dot(f));
+
+  // print(f)
+
+  return final;
 }
 
-function calcShaderBuffer() {
-  shaderBuffer.shader(myShader);
-  myShader.setUniform('tex0', buffer3D);
-  // print(buffer3D.width, buffer3D.height)
-  myShader.setUniform('texSize', [buffer3D.width, buffer3D.height]);
-  shaderBuffer.push();
-  shaderBuffer.rect(0, 0, shaderBuffer.width, shaderBuffer.height);
-  shaderBuffer.pop();
+function setMeltedFace() {
+  // bust.loadPixels();
+  meltedFace.clear();
+  
+  // print(bust.width, bust.height)
+  for (let i = 0; i < bust.pixels.length; i += 4) {
+
+    // get new location
+    let curLoc = new Vec2((i/4) % bust.width, floor((i/4) / bust.width));
+    // print(i, curLoc)
+    let r = curLoc.subtract(x0);
+
+    let newLoc = curLoc.add(getKelvinlet(r));
+
+    let final_i = int((newLoc.getX() + newLoc.getY() * bust.width))*4;
+    // final_i = i;
+
+    // print(i, final_i)
+
+    // Red.
+    meltedFace.pixels[final_i] = bust.pixels[i];
+    // Green.
+    meltedFace.pixels[final_i + 1] = bust.pixels[i+1];
+    // Blue.
+    meltedFace.pixels[final_i + 2] = bust.pixels[i+2];
+    // Alpha.
+    meltedFace.pixels[final_i + 3] = bust.pixels[i+3];
+  }
+  // meltedFace.background(100)
+
+  meltedFace.updatePixels();
 }
+
+// function calcBuffer3D() {
+//   // IMPORTANT!: Needed in order to clear depth buffer
+//   buffer3D.clear();
+//   buffer3D.background(100);
+//   // Note: Can draw a background here with buffer3D.background(...)
+//   //    if transparency is not desired.
+
+//   // Need push() and pop() here because transformations don't get
+//   //   reset each frame.
+//   buffer3D.push();
+//   // (0,0,0) is at the centre of the canvas
+//   // buffer3D.translate(0, cos(r)*(100/factor), 0);
+//   // buffer3D.rotateY(r);
+//   // buffer3D.rotateX(r);
+//   // buffer3D.rotateZ(-r);
+//   // buffer3D.texture(textureBuffer);
+//   // buffer3D.normalMaterial();
+//   // buffer3D.strokeWeight(3);
+//   // draw a 100 by 100 by 100 3D cube
+//   buffer3D.scale(2)
+//   buffer3D.image(bust, 0, 0);
+//   buffer3D.pop();
+
+//   // r += 0.01;
+// }
+
+// function calcShaderBuffer() {
+//   shaderBuffer.shader(myShader);
+//   myShader.setUniform('tex0', buffer3D);
+//   // print(buffer3D.width, buffer3D.height)
+//   myShader.setUniform('texSize', [buffer3D.width, buffer3D.height]);
+//   shaderBuffer.push();
+//   shaderBuffer.rect(0, 0, shaderBuffer.width, shaderBuffer.height);
+//   shaderBuffer.pop();
+// }
 
 function draw() {
   // Draw background image to demonstrate p5.Graphics transparency
   // image(bgImg, 0, 0);
   background(255);
 
+  this.t += 0.01;
+
+  f = new Vec2(sin(this.t), cos(this.t));
+
+  setMeltedFace();
+  // print(meltedFace)
+  image(meltedFace, 0, 0)
+
   // storeTexture();
   // calcBuffer3D();
-  calcShaderBuffer();
+  // calcShaderBuffer();
 
   // Draw the 3D buffer based on textureBuffer and buffer3D
   // upsample buffer3D to window size
@@ -85,7 +190,11 @@ function draw() {
   // image(buffer3D, 0, 0);
   // drawOverlay();
 
-  image(shaderBuffer, -windowWidth/2, -windowHeight/2, shaderBuffer.width/4, shaderBuffer.height/4);
-  canvas.getTexture(shaderBuffer).setInterpolation(NEAREST, NEAREST);
+  // image(shaderBuffer, -windowWidth/2, -windowHeight/2, shaderBuffer.width/4, shaderBuffer.height/4);
+  // canvas.getTexture(shaderBuffer).setInterpolation(NEAREST, NEAREST);
+}
+
+function paramChanged(name) {
+
 }
 
---
+++ facemelt/vec2.js
@@ -0,0 +1,53 @@
+class Vec2 {
+    // Vectors with 2 dimensions, along with associated methods
+    
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    getX() {
+        return this.x;
+    }
+
+    getY() {
+        return this.y;
+    }
+
+    setX(x) {
+        this.x = x;
+    }
+
+    setY(y) {
+        this.y = y;
+    }
+
+    length2() {
+        return sqrt(this.x*this.x + this.y*this.y);
+    }
+
+    add(pos) {
+        return new Vec2(this.x + pos.getX(), this.y + pos.getY());
+    }
+
+    subtract(pos) {
+        return new Vec2(this.x - pos.getX(), this.y - pos.getY());
+    }
+
+    dot(pos) {
+        return this.x*pos.x + this.y*pos.y;
+    }
+
+    det(pos) {
+        return this.x*pos.y - this.y*pos.x;
+    }
+
+    scalarmult(val) {
+        return new Vec2(this.x*val, this.y*val);
+    }
+
+    normalize() {
+        let l = this.length2();
+        return new Vec2(this.x / l, this.y / l);
+    }
+}
